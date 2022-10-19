8ec6df35f3a4765ae27d0c7320e245ca3e52ae52
sonic11829 <sethneal15@ymail.com>
2022-10-18 21:37:26 -0400

    first and final commit

---
+++ views/fruits/Index.jsx
@@ -0,0 +1,39 @@
+const React = require('react');
+const Default = require('../layouts/Default.jsx')
+
+
+class Index extends React.Component{
+    render(){
+        const {fruits} = this.props
+        return(
+            <Default title="Fruits Index Page">
+                <ul>
+                    {
+                        fruits.map((fruit) => {
+                            const {name, color, readyToEat} = fruit
+                            return (
+                                <li key={fruit._id}>
+                                    <a href={`/fruits/${fruit._id}`}>
+                                    {name}</a> is {color}
+                                    
+                                     <br/>
+                                    {
+                                        readyToEat? 
+                                        'It\'s ready to eat':
+                                        'It\'s not ready to eat'
+                                    }
+                                    <br/>
+                                    <form method='POST' action={`/fruits/${fruit._id}?_method=DELETE`}>
+                                        <input type="submit" value={`Delete ${color} ${name}`}/>
+                                    </form>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </Default>
+        )
+    }
+}
+
+module.exports = Index
